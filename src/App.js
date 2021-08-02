734fd36ae3e80ef0da9df95794466a8af900ea39
Sachin Raj <raj.sachin0522@gmail.com>
2021-08-02 22:34:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './Components/todowork/todo';
+
+const App = () => {
+  return (
+    <>
+                     <Todo/>
+      
+    </>
+  )
+}
+
+export default App
+
---
+++ src/todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./todo.css";
+
+function todo() {
+    return (
+        <>
+          <div >SACHIN RAJ            
+        
+              </div>  
+        </>
+    );
+}
+
+export default todo;
